ea75018a4f053a4c1a47686e77b88f55ba7c9db8
Stevenpm25 <gspena51@ucatolica.edu.co>
2025-06-12 04:43:12 -0500

    Arreglos menores

---
+++ models_streamers.py
@@ -0,0 +1,37 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class UserBase(SQLModel):
+    nombre: str = Field(..., min_length=2, max_length=100)
+    origen: str = Field(..., min_length=2, max_length=100)
+    destine: str = Field(..., min_length=2, max_length=100)
+    edad: int = Field(..., ge=0)
+# models_games.py
+
+# models_streamers.py
+
+class UserCreate(SQLModel):
+    nombre: str = Field(..., min_length=2, max_length=100)
+    origen: str = Field(..., min_length=2, max_length=100)
+    destine: str = Field(..., min_length=2, max_length=100)
+    edad: int = Field(..., ge=0)
+
+
+
+
+class User(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class UserWithID(SQLModel):
+    id: int
+    nombre: str
+    origen: str
+    destino: str # Nombre exacto como en la DB
+    edad: int
+class UpdatedStreamer(SQLModel):
+    nombre: str = Field(..., min_length=2, max_length=100)
+    origen: str = Field(..., min_length=2, max_length=100)
+    destine: str = Field(..., min_length=2, max_length=100)
+    edad: int = Field(..., ge=0)
